c7ac8138060979a217df102991abecbafc1ea65a
s0568127 <mwangi.zwyssig@student.htw-berlin.de>
2022-04-08 15:03:55 +0200

    first commit

---
+++ src/main/java/htw/berlin/webtech/wichteln/WichtelnApplication.java
@@ -0,0 +1,13 @@
+package htw.berlin.webtech.wichteln;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WichtelnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WichtelnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/htw/berlin/webtech/wichteln/WichtelnApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.berlin.webtech.wichteln;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WichtelnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
