93b552f7be72e9f90ea4b2bff2f3c45fd7380fed
s0568127 <mwangi.zwyssig@student.htw-berlin.de>
2022-04-20 09:06:22 +0200

    update

---
+++ src/main/java/htw/berlin/webtech/wichteln/person/PersonNotFoundException.java
@@ -0,0 +1,6 @@
+package htw.berlin.webtech.wichteln.person;
+
+public class PersonNotFoundException extends X {
+    public PersonNotFoundException(Long id) {
+    }
+}
---
+++ src/main/java/htw/berlin/webtech/wichteln/person/PersonRepository.java
@@ -0,0 +1,2 @@
+package htw.berlin.webtech.wichteln.person;public interface PersonRepository {
+}
